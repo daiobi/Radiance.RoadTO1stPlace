61207dadc79c082ece999fcde1c3700376962378
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2022-10-03 14:49:17 +0300

    new input system

---
+++ unity/SAAASDSASASaS/Assets/Planet Rover/Scripts/KeyboardMovementTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rover
+{
+    [RequireComponent(typeof(RoverMovement))]
+    public class KeyboardMovementTest : MonoBehaviour
+    {
+        private RoverMovement _rover;
+
+        private void Awake()
+        {
+            _rover = GetComponent<RoverMovement>();
+        }
+
+        private void FixedUpdate()
+        {
+            float acceleration = Input.GetAxis("Vertical");
+            float steer = Input.GetAxis("Horizontal");
+
+            _rover.Move(acceleration, steer);
+        }
+    }
+}
