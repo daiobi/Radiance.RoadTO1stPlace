24715640f1a5730d13d24e7326119f8a1babf236
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2022-10-04 13:59:30 +0300

    rover telemetry

---
+++ unity/SAAASDSASASaS/Assets/Planet Rover/Scripts/RoverBattery.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Rover
+{
+    public class RoverBattery : MonoBehaviour
+    {
+        [SerializeField] private float _maxValue;
+
+        public float Value { get; private set; }
+
+        private void Start()
+        {
+            Value = _maxValue;
+        }
+
+        private void Update()
+        {
+            Value = Mathf.Max(Value - Time.deltaTime, 0);
+        }
+    }
+}
