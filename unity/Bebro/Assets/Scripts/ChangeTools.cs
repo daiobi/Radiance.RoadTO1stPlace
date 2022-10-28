4f99492c8e3867859541d8503d188e8744ecd04a
VisageDvache <82640121+AaaaaaaaAaaaaaaaaaaaaaaaaa4217@users.noreply.github.com>
2022-10-29 02:58:57 +0300

    Xyebes

---
+++ unity/Bebro/Assets/Scripts/ChangeTools.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTools : MonoBehaviour
+{
+
+    private bool _drill;
+    private bool _arm;
+    private bool _rover_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _drill = false;
+        _arm = false;
+        _rover_ = false;
+    }
+
+    // Update is called once per frame
+    public void DrillActive()
+    {
+        _drill = !_drill;
+        _arm = false;
+        _rover_ = false;
+    }
+
+    public void ArmActive()
+    {
+        _arm = !_arm;
+        _drill = false;
+        _rover_ = false;
+    }
+
+    public void RoverActive()
+    {
+        _rover_ = !_rover_;
+        _drill = false;
+        _arm = false;
+    }
+
+    private void Update()
+    {
+        if (_rover_ == true)
+        {
+            ///
+        }
+        else if (_arm == true)
+        {
+            ///
+        }
+
+        else if(_drill == true)
+        {
+            ///
+        }
+    }
+}
---
+++ unity/Bebro/Assets/Scripts/PhotoMake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoMake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera _PhotoCamera;
+    public RawImage _MakedPhoto;
+    public void MakePhoto()
+    {
+        _PhotoCamera.GetComponent<Camera>().Render();
+        _MakedPhoto.texture = _PhotoCamera.activeTexture;
+    }
+}
