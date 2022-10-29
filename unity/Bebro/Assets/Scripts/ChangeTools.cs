8fe049fd24c003e8edfc666af39055a33b180280
VisageDvache <82640121+AaaaaaaaAaaaaaaaaaaaaaaaaa4217@users.noreply.github.com>
2022-10-29 10:55:41 +0300

    zxczxczxczxczxczxcvzxc

--- unity/Bebro/Assets/Scripts/ChangeTools.cs
+++ unity/Bebro/Assets/Scripts/ChangeTools.cs
@@ -7,13 +7,12 @@ public class ChangeTools : MonoBehaviour
 
     private bool _drill;
     private bool _arm;
-    private bool _rover_;
+    //private bool _rover_;
     // Start is called before the first frame update
     void Start()
     {
         _drill = false;
         _arm = false;
-        _rover_ = false;
     }
 
     // Update is called once per frame
@@ -21,26 +20,24 @@ public class ChangeTools : MonoBehaviour
     {
         _drill = !_drill;
         _arm = false;
-        _rover_ = false;
     }
 
     public void ArmActive()
     {
         _arm = !_arm;
         _drill = false;
-        _rover_ = false;
     }
 
-    public void RoverActive()
-    {
-        _rover_ = !_rover_;
-        _drill = false;
-        _arm = false;
-    }
+    //public void RoverActive()
+    //{
+    //    _rover_ = !_rover_;
+    //    _drill = false;
+    //    _arm = false;
+    //}
 
     private void Update()
     {
-        if (_rover_ == true)
+        if (_arm == false && _drill == false)
         {
             ///
         }
