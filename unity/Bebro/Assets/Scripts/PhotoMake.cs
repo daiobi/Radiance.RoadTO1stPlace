bbf1b1a727b393ee54ae3edc389d93d7394c991a
VisageDvache <82640121+AaaaaaaaAaaaaaaaaaaaaaaaaa4217@users.noreply.github.com>
2022-10-29 11:12:23 +0300

    zxqwert

--- unity/Bebro/Assets/Scripts/PhotoMake.cs
+++ unity/Bebro/Assets/Scripts/PhotoMake.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PhotoMake : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Camera _PhotoCamera;
-    public RawImage _MakedPhoto;
-    public void MakePhoto()
+    static public Texture2D GetRTPixels(RenderTexture rt)
     {
-        _PhotoCamera.GetComponent<Camera>().Render();
-        _MakedPhoto.texture = _PhotoCamera.activeTexture;
+        //_PhotoCamera.GetComponent<Camera>().Render();
+        //_MakedPhoto.texture = _PhotoCamera.activeTexture;
+        RenderTexture currentActiveRT = RenderTexture.active;
+        RenderTexture.active = rt;
+        Texture2D tex = new Texture2D(rt.width, rt.height);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+
+        // Restorie previously active render texture
+        RenderTexture.active = currentActiveRT;
+        return tex;
     }
 }
